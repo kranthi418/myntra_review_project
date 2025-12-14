3ec147a3d25c8b36a9baa484b6df2ff6224587b4
kranthi418 <kranthinaik09@gmail.com>
2025-12-14 18:26:56 +0530

    done

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://kranthi:NwtPxSWouijDe4hp@cluster0.dqik7.mongodb.net/?appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
